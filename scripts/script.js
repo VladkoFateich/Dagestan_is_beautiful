a1a505361500d8543ab274cfa93e61b93f4aa1fc
Vladko Fateich <Fateev_wbd@mail.ru>
2025-01-16 20:06:08 +0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', () => {
+  // ------------------------------------ program ------------------------------------
+
+  const programs = document.querySelectorAll('.tours__program'),
+    content = document.querySelectorAll('.tours__description'),
+    tours = document.querySelector('.tours__programs');
+
+  function hideContent() {
+    content.forEach((item) => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+    programs.forEach((item) => {
+      item.classList.remove('tours__program_active');
+    });
+  }
+  function showContent(i = 0) {
+    content[i].classList.add('show', 'fade');
+    content[i].classList.remove('hide');
+    programs[i].classList.add('tours__program_active');
+  }
+  hideContent();
+  showContent();
+
+  tours.addEventListener('click', (e) => {
+    const target = e.target;
+    console.log(target.classList);
+    if (target && target.classList.contains('tours__program')) {
+      programs.forEach((item, i) => {
+        if (target === item) {
+          hideContent();
+          showContent(i);
+        }
+      });
+    }
+  });
+});
